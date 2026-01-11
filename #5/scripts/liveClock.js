7d2b2c5f2c4a3aa585bf48985d72fb3dd542a451
Ahmed Wael <notahmedwael888@gmail.com>
2026-01-11 19:25:07 +0200

    live clock can now be used multiple times instead of once

--- #5/scripts/liveClock.js
+++ #5/scripts/liveClock.js
@@ -1,33 +1,32 @@
 const startClock = document.querySelector("#start-clock");
 const stopText = document.querySelector("p.stop");
+const clockDisplay = document.querySelector('#clock p');
 let clockInterval;
 
 const updateClock = () => {
     const now = new Date();
     const timeString = now.toLocaleTimeString();
-    const clockDisplay = document.querySelector('#clock p');
-    
     if (clockDisplay) {
         clockDisplay.textContent = timeString;
     }
 };
 
 startClock.addEventListener('click', () => {
+    clockDisplay.style.color = 'black';
     startClock.style.display = 'none';
     stopText.style.display = 'block';
     updateClock();
     clockInterval = setInterval(updateClock, 1000);
-}, { once: true });
+});
 
 document.addEventListener('keydown', (event) => {
     if (event.altKey && event.key.toLowerCase() === 'w') {
         event.preventDefault();
         
         clearInterval(clockInterval);
-        console.log("Clock stopped via Alt + W");
         
-        const clockDisplay = document.querySelector('#clock p');
         if (clockDisplay) clockDisplay.style.color = 'red';
         stopText.style.display = 'none';
+        startClock.style.display = 'block';
     }
 });
