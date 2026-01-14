238f94507a5ad53b8d196e8ee870c76342d68027
Ahmed Wael <notahmedwael888@gmail.com>
2026-01-15 01:11:56 +0200

    added es6+ array methods task

---
+++ 6/Array/newArrayMethods.js
@@ -0,0 +1,17 @@
+var fruits = ["apple", "strawberry", "banana", "orange", "mango"];
+
+const firstTest = fruits.every(s => typeof s === "string");
+
+console.log(firstTest);
+
+const secondTest = fruits.some(c => c.charAt(0) === 'a');
+
+console.log(secondTest);
+
+const filteredArray = fruits.filter(fruit=> fruit.charAt(0) === "b" || fruit.charAt(0) === "s");
+
+console.log(filteredArray);
+
+const iLikeFruits = fruits.map(fruit=> `I like ${fruit}`);
+
+console.log(iLikeFruits);
